4513b53383ba8ddf6cfa94e485092f453b2bb4c8
Arthur Soares Pinto <arthursoares.comercial@gmail.com>
2022-11-05 20:03:33 -0300

    correção de fotos

--- js/dados.js
+++ js/dados.js
@@ -1,16 +1,18 @@
 var dados = [
-    {'name':'Persiana celular', 'description':'Melhore a luminosidade de seu ambiente','img':'images/galeria/img-01.jpg'},
-    {'name':'Rollo', 'description':'Persianas Rolo','img':'images/galeria/img-03.jpg'},
-    {'name':'Rollo', 'description':'Persianas Rolo','img':'images/galeria/img-05.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-06.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-10.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-12.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-13.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-14.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-21.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-22.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-23.jpg'},
-    {'name':'Rolô', 'description':'Persianas Rolo','img':'images/galeria/img-24.jpg'}
+    {'name':'Persiana celular', 'description':'Solicite o orçamento','img':'images/galeria/img-01.jpg', 'href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Rollo', 'description':'R$ 260,00/m²','img':'images/galeria/img-03.jpg', 'href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Persiana Horizontal 50mm em Madeira', 'description':'Solicite o orçamento','img':'images/galeria/img-05.jpg', 'href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Persiana Vertical em Tecido', 'description':'R$ 180,00/m²','img':'images/galeria/img-06.jpg','href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Persiana Horizontal 25mm em Aluminio', 'description':'R$ 180,00/m²','img':'images/galeria/img-12.jpeg','href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Sheer Elegance | Double Vision', 'description':'R$ 350,00/m²','img':'images/galeria/img-21.jpg','href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Cortina Painel', 'description':'R$ 300,00/m²','img':'images/galeria/img-27.jpeg','href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'},
+    {'name':'Romana', 'description':'R$ 380,00/m²','img':'images/galeria/img-22.jpeg','href':'https://api.whatsapp.com/send?phone=553131407242&text=Olá!%20Encontrei%20seu%20site%20na%20Internet%20e%20gostaria%20de%20um%20orçamento%20para%20'}
+    // {'name':'Rolô', 'description':'Solicite o orçamento','img':'images/galeria/img-10.jpg'},
+    // {'name':'Rolô', 'description':'Solicite o orçamento','img':'images/galeria/img-13.jpg'},
+    // {'name':'Rolô', 'description':'Solicite o orçamento','img':'images/galeria/img-14.jpg'},
+    // {'name':'Rolô', 'description':'Solicite o orçamento','img':'images/galeria/img-23.jpg'},
+    // {'name':'Rolô', 'description':'Solicite o orçamento','img':'images/galeria/img-24.jpg'}
+    
 ]
 
 
@@ -27,13 +29,18 @@ function onload(){
                     <div class="overlay-content">
                         <div class="overlay-text">
                             <div class="folio-info">
-                            <h3>`+dados[i].name+`</h3>
-                            <p>`+dados[i].description+`</p>
+                            <h3 style="color:orange;">`+dados[i].name+`</h3>
+                            <p> A partir de:<br><h3><Strong><span style="color:orange;">`+dados[i].description+`</span></Strong></h3></p>
                             </div>
                             <div class="folio-overview">
                             <span class="folio-link">
                                 <a class="folio-read-more" href="#" data-single_url="portfolio-single.html" >
-                                    <i class="fa fa-link"></i>
+                                    <i class="fa fa-link my-float"></i>  
+                                </a>
+                            </span>
+                            <span class="folio-expand">
+                                <a href="`+dados[i].href+dados[i].name+`">
+                                    <i style="color:green;" class="fa fa-whatsapp my-float"></i>  
                                 </a>
                             </span>
                             <span class="folio-expand">
