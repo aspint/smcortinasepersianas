cd00360459bd25068acebdbc1162b22453cc42f8
Arthur Soares Pinto <arthursoares.comercial@gmail.com>
2022-11-05 20:20:53 -0300

    correção telefone com whatsapp e sendmail php

--- sendemail.php
+++ sendemail.php
@@ -1,20 +1,28 @@
 <?php
-$name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email'])); 
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$tel        = @trim(stripslashes($_POST['tel']));
-$message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'arthursoares.comercial@gmail.com';//replace with your email
+  $nome = $_POST['nome'];
+  $email= $_POST['email'];
+  $mensagem= $_POST['subject'];
+  $to = "arthursoares.comercial@gmail.com";
+  $assunto = "Mensagem de ".$email.com
+  mail($to,$assunto,$mensagem);
 
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
-$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
 
-mail($to, $subject, $message+' /n Telefone: '+$tel, $headers);
+// $name       = @trim(stripslashes($_POST['name'])); 
+// $from       = @trim(stripslashes($_POST['email'])); 
+// $subject    = @trim(stripslashes($_POST['subject'])); 
+// $tel        = @trim(stripslashes($_POST['tel']));
+// $message    = @trim(stripslashes($_POST['message'])); 
+// $to   		= 'arthursoares.comercial@gmail.com';//replace with your email
 
-die;
+// $headers   = array();
+// $headers[] = "MIME-Version: 1.0";
+// $headers[] = "Content-type: text/plain; charset=iso-8859-1";
+// $headers[] = "From: {$name} <{$from}>";
+// $headers[] = "Reply-To: <{$from}>";
+// $headers[] = "Subject: {$subject}";
+// $headers[] = "X-Mailer: PHP/".phpversion();
+
+// mail($to, $subject, $message+' /n Telefone: '+$tel, $headers);
+
+// die;
 ?>
