be01a351840d158a02d3edbbed14d70a60515600
Arthur Soares Pinto <arthursoares.comercial@gmail.com>
2022-11-06 00:23:48 -0300

    correções para envio do corpo do email

--- enviar.php
+++ enviar.php
@@ -27,7 +27,7 @@
  // DEFINIÇÃO DA MENSAGEM
  $email = $_POST['email'];
  $mail->Subject  = "Formulário de Contato"; // Assunto da mensagem
- $mail->Body = $_POST['name'] ; //." <br>"; // Texto da mensagem
+ $mail->Body = " Nome: ".$_POST['name'] . "<br>"; //." <br>"; // Texto da mensagem
  $mail->Body .= " E-mail: ".$_POST['email']."<br>"; // Texto da mensagem
  $mail->Body .= " Assunto: ".$_POST['subject']."<br>"; // Texto da mensagem
  $mail->Body .= " Mensagem: ".nl2br($_POST['message+'])."<br>"; // Texto da mensagem
