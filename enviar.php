83b13d962480082b1047c9cf147f2f186ccf2366
Arthur Soares Pinto <arthursoares.comercial@gmail.com>
2022-11-06 01:33:20 -0300

    teste com copia e dupla linha branca

--- enviar.php
+++ enviar.php
@@ -19,23 +19,21 @@
  // DADOS DO DESTINATÁRIO
  $mail->AddAddress('formweb@smcortinasepersianas.com.br', 'Nome - Recebe1'); // Define qual conta de email receberá a mensagem
  //$mail->AddAddress('recebe2@dominio.com.br'); // Define qual conta de email receberá a mensagem
- //$mail->AddCC('copia@dominio.net'); // Define qual conta de email receberá uma cópia
+ $mail->AddCC($_POST['email']); // Define qual conta de email receberá uma cópia
  //$mail->AddBCC('copiaoculta@dominio.info'); // Define qual conta de email receberá uma cópia oculta
  // Definição de HTML/codificação
  $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
  $mail->CharSet = 'utf-8'; // Charset da mensagem (opcional)
  // DEFINIÇÃO DA MENSAGEM
  $mail->Subject  = "Formulário de Contato"; // Assunto da mensagem
- $mail->Body = " Nome: ".$_POST['name'] . "<br>"; //." <br>"; // Texto da mensagem
- $mail->Body .= " E-mail: ".$_POST['email']."<br>"; // Texto da mensagem
- $mail->Body .= " Assunto: ".$_POST['subject']."<br>"; // Texto da mensagem
- $mail->Body .= " Telefone: ".$_POST['tel']."<br>"; // Texto da mensagem
- $mail->Body .= " Mensagem: ".nl2br($_POST['message'])."<br>"; // Texto da mensagem
+ $mail->Body = " Nome: ".$_POST['name'] . "<br><br>"; //." <br>"; // Texto da mensagem
+ $mail->Body .= " E-mail: ".$_POST['email']."<br><br>"; // Texto da mensagem
+ $mail->Body .= " Assunto: ".$_POST['subject']."<br><br>"; // Texto da mensagem
+ $mail->Body .= " Telefone: ".$_POST['tel']."<br><br>"; // Texto da mensagem
+ $mail->Body .= " Mensagem: ".nl2br($_POST['message'])."<br><br>"; // Texto da mensagem
  // ENVIO DO EMAIL
  $enviado = $mail->Send();
  // Limpa os destinatários e os anexos
- $mail->AddAddress($_POST['email'], 'Nome - Recebe1');
- $mail->Body = " Email Recebido em breve entraremos em contato";
  $mail->ClearAllRecipients();
  // Exibe uma mensagem de resultado do envio (sucesso/erro)
  if ($enviado) {
